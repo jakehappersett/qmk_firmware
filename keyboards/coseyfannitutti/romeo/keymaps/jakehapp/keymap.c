5042da1439de6cdd2d3c171ce975484ffaff0b6d
Jake <jake.happersett@gmail.com>
2020-04-19 20:16:28 -0400

    fixed issues with keymap

--- keyboards/coseyfannitutti/romeo/keymaps/jakehapp/keymap.c
+++ keyboards/coseyfannitutti/romeo/keymaps/jakehapp/keymap.c
@@ -15,6 +15,9 @@
  */
 #include QMK_KEYBOARD_H
 
+extern keymap_config_t keymap_config;
+
+
 #define _QW 0
 #define _NAV 1
 #define _NUM 2
@@ -31,28 +34,28 @@
 #define COLO LT(_NUM, KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QW] = LAYOUT( /* Qwerty */
+  [_QW] = LAYOUT_ansi_split_space( /* Qwerty */
     KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     L_ESC,   KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,
     KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, 
     KC_LCTL, MO(_NUM), KC_LALT,          KC_ENT,  COLO,    KC_SPC,                    KC_RGUI, KC_ESC,  TG(_FUNC)
     ),
-  [_NAV] = LAYOUT( /* LAYER 1 */
+  [_NAV] = LAYOUT_ansi_split_space( /* LAYER 1 */
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN,  KC_RPRN,   KC_DEL,
     _______, KC_BSLS, KC_QUOT, KC_MINS, KC_EQL,  _______, KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT, KC_F5,
     _______, KC_ESC,  _______, KC_PSCR, L_CURBR, R_CURBR, KC_LBRC, KC_RBRC,  KC_HOME, KC_END,   KC_INSERT,
     _______, KC_LGUI, _______,          _______, _______, _______,                    _______,  _______,   _______
   ),
-  [_NUM] = LAYOUT( /* LAYER 2 */
+  [_NUM] = LAYOUT_ansi_split_space( /* LAYER 2 */
     KC_TILD,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5 ,   KC_6,    KC_7,    KC_8,   KC_9,    KC_0,     _______,
-    KC_ESC,    KC_PIPE, KC_DQUO, KC_UNDS, KC_PLUS, L_CURBR, R_CURBR, KC_4,    KC_5,   KC_6,    _______,  KC_ENT,
-    _______,   _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,   KC_3,    _______,  _______,
+    KC_ESC,    KC_PIPE, KC_DQUO, KC_UNDS, KC_PLUS, L_CURBR, R_CURBR, KC_4,    KC_5,   KC_6,    _______,  
+    _______,   _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,   KC_3,    _______,  
     _______,   _______, _______,          _______,_______, _______,                   KC_VOLU, KC_VOLD,  _______
   ),
-  [_FUNC] = LAYOUT( /* LAYER 3 */
+  [_FUNC] = LAYOUT_ansi_split_space( /* LAYER 3 */
     _______, _______, _______, _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   
+    _______, _______, _______, _______, _______, _______, _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  
     _______, _______, _______,          _______, _______, _______,                   _______, _______, _______
   ) 
 };
