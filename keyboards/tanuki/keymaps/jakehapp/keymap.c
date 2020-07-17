caa88886fe23cf854167f47f66c07fafb5f64d91
Jake <jake.happersett@gmail.com>
2020-07-17 13:08:45 -0400

    fix missing inputs

--- keyboards/tanuki/keymaps/jakehapp/keymap.c
+++ keyboards/tanuki/keymaps/jakehapp/keymap.c
@@ -30,8 +30,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QW] = LAYOUT(
     KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,     KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     L_ESC ,   KC_A,     KC_S,     KC_D,    KC_F,    KC_G,     KC_H,       KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOTE, \
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,     KC_N,       KC_M,    KC_QUOT, KC_SLSH, MO(_NUM),  \
-    KC_LCTL,  MO(_NUM), KC_COMMA, LCTL_T(KC_ENT),   KC_SPC,               KC_RGUI,  TG(_FUNC)),
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,     KC_N,       KC_M,    KC_COMMA, KC_DOT, LT(_NUM, KC_SLSH),  \
+    MO(_NUM), KC_LCTL,      KC_LALT, LCTL_T(KC_ENT),   KC_SPC,               KC_RGUI,  TG(_FUNC)),
 
 [_NAV] = LAYOUT(
     KC_GRV,   KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,\
@@ -41,15 +41,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_NUM] = LAYOUT(
     KC_TILD,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,    KC_TRNS,\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_4,     KC_5,     KC_6,     KC_TRNS, KC_TRNS, \
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_1,     KC_2,     KC_3,     KC_TRNS,  \
+    KC_TRNS,  KC_PIPE,  KC_DQUO,  KC_UNDS,  KC_PLUS,  KC_TRNS,  KC_TRNS,  KC_4,     KC_5,     KC_6,     KC_TRNS, KC_TRNS, \
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_0,  KC_1,     KC_2,     KC_3,     KC_TRNS,  \
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS),
 
 [_FUNC] = LAYOUT(
-    KC_TRNS,  RGB_MOD,  RGB_HUI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_TRNS, \
+    RESET,  RGB_MOD,  RGB_HUI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_TRNS, \
     KC_TRNS,  RGB_SAI,  RGB_VAI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_TRNS, \
     KC_TRNS,  RGB_VAD,  RGB_SAD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_F9,  KC_F10, KC_F11,  KC_F12,  \
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,           KC_TRNS,  KC_TRNS)
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,           CUSTRGB,  KC_TRNS)
   
 };
 
@@ -67,8 +67,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     if(user_config.layer_rgb) {
         switch (get_highest_layer(state)) {
             case _QW:
-                rgblight_sethsv_noeeprom(0,10,255);
-                rgblight_mode_noeeprom(1);
+                rgblight_sethsv_noeeprom(HSV_MAGENTA);
+                rgblight_mode_noeeprom(9);
                 break;
             case _NAV:
                 rgblight_sethsv_noeeprom(130,200,255);
@@ -79,7 +79,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
                 rgblight_mode_noeeprom(1);
                 break;
             case _FUNC:
-                rgblight_sethsv_noeeprom(0,180,255);
+                rgblight_sethsv_noeeprom(HSV_GOLDENROD);
                 rgblight_mode_noeeprom(1);
                 break;
         }
