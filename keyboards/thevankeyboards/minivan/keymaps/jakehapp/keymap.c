b8a0542478a0d072b007a6a3c264199144b1b5e0
Jake <jake.happersett@gmail.com>
2020-07-25 10:12:35 -0400

    make return ctl

--- keyboards/thevankeyboards/minivan/keymaps/jakehapp/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/jakehapp/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     L_ESC,   KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, R_ENT,
     KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(_NUM),
-    KC_LCTL, MO(_NUM), KC_LALT,                   KC_ENT,  KC_SPC,                    KC_RGUI, KC_ESC,  TG(_FUNC)
+    KC_LCTL, MO(_NUM), KC_LALT,           LCTL_T(KC_ENT),  KC_SPC,                    KC_RGUI, KC_ESC,  TG(_FUNC)
   ),
 /*!end*/
 //added sensible vim controls
