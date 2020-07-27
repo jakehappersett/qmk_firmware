5a048372c92dc782f00e41425a33c279cb229467
Jake <jake.happersett@gmail.com>
2020-07-27 15:38:45 -0400

    modify keymap

--- keyboards/coseyfannitutti/romeo/keymaps/jakehapp/keymap.c
+++ keyboards/coseyfannitutti/romeo/keymaps/jakehapp/keymap.c
@@ -34,8 +34,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
     KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     L_ESC,   KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,
-    KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, 
-    KC_LCTL, MO(_NUM), KC_LALT,          KC_ENT,  COLO,    KC_SPC,                    KC_RGUI, KC_ESC,  TG(_FUNC)
+    KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  LT(_NAV,KC_SLASH), 
+    KC_LCTL, MO(_NUM), KC_LALT,   LCTL_T(KC_ENT), COLO,    KC_SPC,                    KC_RGUI, KC_ESC,  TG(_FUNC)
     ),
   [_NAV] = LAYOUT( /* LAYER 1 */
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN,  KC_RPRN,   KC_DEL,
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,   _______, _______,          _______,_______, _______,                   KC_VOLU, KC_VOLD,  _______
   ),
   [_FUNC] = LAYOUT( /* LAYER 3 */
-    _______, _______, _______, _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,
+    RESET,   _______, _______, _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,
     _______, _______, _______, _______, _______, _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,
     _______, _______, _______, _______, _______, _______, _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
     _______, _______, _______,          _______, _______, _______,                   _______, _______, _______
