1ba25f648f6fc91ed4b03fd6e9a311d606cfce15
Jake Happersett <jake.happersett@gmail.com>
2020-09-12 14:39:02 -0400

    Enable bootmagic on minivan

--- keyboards/thevankeyboards/minivan/keymaps/jakehapp/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/jakehapp/keymap.c
@@ -11,6 +11,7 @@ extern keymap_config_t keymap_config;
 #define _NAV 1
 #define _NUM 2
 #define _FUNC 3
+#define _MAC 4
 
 // Curly braces have their own keys. These are defined to make them not mess up
 // the grid in layer 2.
@@ -54,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RESET,   _______, _______, _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,
     KC_ESC,  _______, _______, _______, _______, _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,
     KC_LSFT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
-    _______, KC_LSFT, KC_B,                      KC_SPC,  KC_C,                      _______, _______, _______
+    CG_SWAP, KC_LSFT, KC_B,                      KC_SPC,  KC_C,                      _______, _______, _______
   )
 /*!end*/
 };
